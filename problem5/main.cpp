0e606b322e91edc2fa937475a7a5d1237452881d
Dan <dlangridge@gmail.com>
2014-08-26 23:25:06 -0700

    Added improved algorithm for problem5

--- problem5/main.cpp
+++ problem5/main.cpp
@@ -6,10 +6,31 @@
 
 using namespace std;
 
-int largestDivisibleNumber_Naive(int bound) {
+unsigned const MAX = 10000;
+// If the number is not dvisible by the largest number, then n + 1, .. n + largest number - 1 wil not be divisible either
+int smallestDivisibleNumber_Improved(int bound) {
   int MaxNum = bound;
   int divisibleFlag = 0;
-  for (unsigned i = MaxNum; i < 100000; i++) {
+  for (unsigned i = MaxNum; i < MAX; i + bound ) {
+    for (unsigned j = bound - 1; j < 1; j--) {
+      if ( MaxNum % j > 0) {
+        divisibleFlag = 1;
+        break;
+      }
+    }
+    if ( divisibleFlag == 0) {
+      MaxNum = i;
+    }
+    divisibleFlag = 0;
+  }
+  if (MaxNum == MAX) return 0;
+  return MaxNum;
+}
+
+int smallestDivisibleNumber_Naive(int bound) {
+  int MaxNum = bound;
+  int divisibleFlag = 0;
+  for (unsigned i = MaxNum; i < MAX; i++) {
     for (unsigned j = 1; j < bound; j++) {
       if ( MaxNum % j > 0) {
         divisibleFlag = 1;
@@ -21,11 +42,12 @@ int largestDivisibleNumber_Naive(int bound) {
     }
     divisibleFlag = 0;
   }
-  if (MaxNum == 100000) return 0;
+  if (MaxNum == MAX) return 0;
   return MaxNum;
 }
 
 int main(int argv, char** argc) {
-  cout << largestDivisibleNumber_Naive(20);
+  cout << smallestDivisibleNumber_Naive(20);
+  cout << smallestDivisibleNumber_Improved(20);
   return 0;
 }
