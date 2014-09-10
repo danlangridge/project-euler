518833d001c7dee344e285981af5e0d2cf29ad23
Dan <dlangridge@gmail.com>
2014-09-09 23:16:56 -0700

    added first part of problem49

---
+++ problem49/main.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+
+using namespace std;
+
+bool guaranteedPrimeTest_Improved(int i) {
+  if (n <= 3 && n >= 1) return true;
+  if ( n % 2 == 0 || n % 3 == 0) return false;
+  for (int i = 5; i < sqrt(n) + 1; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
+  } 
+  return true;
+}
+
+bool probabilisticPrimeTest(int i, int conviction) {
+  srand(time(NULL));
+  for (unsigned a = rand() % i; conviction > 0; conviction--) {
+    if ((a^(i-1)) % i != 1) return false;
+  }
+  return true;
+}
+
+int findFirstPrime(int start) {
+  for (unsigned i = start i < start*10; i++) {
+    if (probilisticPrimeTest(i, 100)) return i; 
+  }
+  return 0;
+}
+
+string primeSequence() {
+  int min = 1000;
+  int firstPrime = findFirstPrime(1000);
+  int currentNumber = 0;
+  int difference = 0;
+
+  for (unsigned j = firstPrime +1; j  
+  for (unsigned i = firstPrime + 1;i < min*10; i++) {
+    if (probabilisticPrimeTest(i,100))  
+  } 
+}
+
+int main(int argv, char** argc) { 
+  return 0;
+}
