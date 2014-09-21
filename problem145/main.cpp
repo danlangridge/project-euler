ecfd3371f84c9d826717b6c9609a38a07aa94081
Dan <dlangridge@gmail.com>
2014-09-20 23:38:31 -0700

    Added problem145

--- problem145/main.cpp
+++ problem145/main.cpp
@@ -8,10 +8,14 @@
 
 using namespace std;
 
-int reverseNumber(double number) {
+inline string convertToString(double number) {
   stringstream ss;
   ss << number;
-  string snumb = number.c_str(); 
+  return  ss.c_str(); 
+}
+
+int reverseNumber(double number) {
+  snumb = convertToString(number); 
   for (unsigned i = 0, j = snumb.length(); i < snumb.length()/2; i++) {
     char temp = snumb[i];
     snumb[i] = snumb[j];
@@ -20,6 +24,22 @@ int reverseNumber(double number) {
   return atoi(number);
 }
 
+bool hasOddDigits(int number) {
+  snumb = convertToString(number);
+  for (unsigned i = 0; i < snumb.length(); i++) {
+    if(!(atoi(snumb[i]) % 2)) return false;
+  }
+  return true;
+}
+
+int numberOfReversibleDigits(int limit) {
+  int count = 0; 
+  for (unsigned i = 0; i < limit; i++) {
+    if (hasOddDigits(i) && hasOddDigits(reverseNumber(i)) && hasOddDigits(i + reverseNumber(i)))
+      count++;
+  }
+  return count;
+}
 
 void printResult(double[] resultArray) {
   for (unsigned i = 0; i < sizeof(resultArray); i++) {
