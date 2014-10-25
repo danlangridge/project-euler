f53f5789f11cb296b552f9c845b63f7bac2fb1d2
Dan <dlangridge@gmail.com>
2014-10-24 23:58:04 -0700

    added problem119

---
+++ problem119/main.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+
+using namespace std;
+
+bool guaranteedPrimeTest_Improved(int i) {
+  if (n <= 3 && n >= 1) return true;
+  if ( n % 2 == 0 || n % 3 == 0) return false;
+  for (int i = 5; i < sqrt(n) + 1; i += 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false;
+  } 
+  return true;
+}
+
+int digitSum(int num) {
+  int digit = log10(num);
+  stringstream ss;
+  ss << num;
+  string numb = ss.c_str(); 
+  int sum = 0; 
+  for ( unsigned i = 0; i < digit; i++) {
+    sum += numb[i]; 
+  }
+  return sum;
+}
+
+int sequence(int n, int limit) {
+  int a = 0;
+
+  for (unsigned i =0; i < limit; i++) {
+    if ( guaranteedPrimeTest_Improved(i) {
+      int sum = digitSum;
+      for (unsigned j = 0; sum^j < i + 1; j++) {
+        if (sum^j ==  
+      } 
+    }
+  } 
+
+} 
+
+
+
+int main(int argv, char** argc) { 
+  return 0;
+}
